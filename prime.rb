7adafc1c64ca786fd43c9e38ff076ab47c32bc26
Nate Miller <natekmiller96@gmail.com>
2018-06-23 17:24:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+
+def prime?(n)
+  if n < 2 && n > -1
+    return false
+  elsif n == 2 || n == -2
+    return true
+  else
+    if n % 2 == 0
+      puts "f"
+      return false
+    end
+    count = n/2
+    a = 1
+    while count != 0
+      a += 1
+      if n % a == 0
+        puts "f"
+        return false
+      else
+        count -= 1
+      end
+    end
+    puts "t"
+    return true
+  end
+end
+prime?(13)
